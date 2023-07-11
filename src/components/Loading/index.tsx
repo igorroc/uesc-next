e77b4b4fa3c0d36a51f62bcf600d3e6144a3c73e
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-11 10:53:11 -0300

    feat: Loading component

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import styles from "./loading.module.css"
+
+export default function Loading() {
+	return (
+		<div className={styles.loading}>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+			<div></div>
+		</div>
+	)
+}
