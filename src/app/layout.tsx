1ddb4846d9ce45a4c13ca9760cc8a0217d0d2bba
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-11 10:23:55 -0300

    fix: Default main wrapper

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 	return (
 		<html lang="pt-br">
 			<body className={inter.className}>
-				{children}
+				<main>{children}</main>
 				<Footer />
 			</body>
 		</html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,16 @@ import { FaCalculator } from "react-icons/fa"
 export default function Home() {
 	return (
 		<>
-			<main className={styles.main}>
-				<div className={styles.wrapper}>
-					<Link href="/emails" className={styles.links}>
-						<BsFillEnvelopeFill size={48} />
-						<span>Banco de E-mails</span>
-					</Link>
-					<Link className={styles.links} href="./calculadora">
-						<FaCalculator size={48} />
-						<span> Calculadora de Notas </span>
-					</Link>
-				</div>
-			</main>
+			<div className={styles.wrapper}>
+				<Link href="/emails" className={styles.links}>
+					<BsFillEnvelopeFill size={48} />
+					<span>Banco de E-mails</span>
+				</Link>
+				<Link className={styles.links} href="./calculadora">
+					<FaCalculator size={48} />
+					<span> Calculadora de Notas </span>
+				</Link>
+			</div>
 		</>
 	)
 }
