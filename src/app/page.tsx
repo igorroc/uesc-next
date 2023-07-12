398801b1e72cc6c544b7c40aad119add8a25c8b5
Igor Rocha <igor_roc@hotmail.com.br>
2023-07-12 17:01:56 -0300

    fix: Style centered

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,22 +7,24 @@ import { FaCalculator } from "react-icons/fa"
 export default function Home() {
 	return (
 		<>
-			<div className={styles.wrapper}>
-				<Link href="/emails" className={styles.links}>
-					<BsFillEnvelopeFill size={48} />
-					<span>Banco de E-mails</span>
-				</Link>
-				<Link className={styles.links} href="./calculadora">
-					<FaCalculator size={48} />
-					<span> Calculadora de Notas </span>
-				</Link>
-				<Link
-					className={styles.links}
-					href="https://www.prograd.uesc.br/PortalSagres/Acesso.aspx"
-				>
-					<BsGlobeAmericas size={48} />
-					<span> Portal Sagres </span>
-				</Link>
+			<div className={styles.pageCenter}>
+				<div className={styles.wrapper}>
+					<Link href="/emails" className={styles.links}>
+						<BsFillEnvelopeFill size={48} />
+						<span>Banco de E-mails</span>
+					</Link>
+					<Link className={styles.links} href="./calculadora">
+						<FaCalculator size={48} />
+						<span> Calculadora de Notas </span>
+					</Link>
+					<Link
+						className={styles.links}
+						href="https://www.prograd.uesc.br/PortalSagres/Acesso.aspx"
+					>
+						<BsGlobeAmericas size={48} />
+						<span> Portal Sagres </span>
+					</Link>
+				</div>
 			</div>
 		</>
 	)
