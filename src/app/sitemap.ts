4c0f84fd1b2407d914672d5641e18d8fcc4d2065
Igor MSI <igor_roc@hotmail.com.br>
2026-05-26 20:45:18 -0300

    feat: Remove change frequency and priority from sitemap entries

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -9,38 +9,26 @@ export default function sitemap(): MetadataRoute.Sitemap {
 		{
 			url: `${baseUrl}/`,
 			lastModified: now,
-			changeFrequency: "weekly",
-			priority: 1,
 		},
 		{
 			url: `${baseUrl}/calculadora`,
 			lastModified: now,
-			changeFrequency: "weekly",
-			priority: 0.9,
 		},
 		{
 			url: `${baseUrl}/calculadora/info`,
 			lastModified: now,
-			changeFrequency: "monthly",
-			priority: 0.7,
 		},
 		{
 			url: `${baseUrl}/emails`,
 			lastModified: now,
-			changeFrequency: "weekly",
-			priority: 0.9,
 		},
 		{
 			url: `${baseUrl}/emails/add`,
 			lastModified: now,
-			changeFrequency: "monthly",
-			priority: 0.6,
 		},
 		{
 			url: `${baseUrl}/privacidade`,
 			lastModified: now,
-			changeFrequency: "yearly",
-			priority: 0.4,
 		},
 	]
 }
